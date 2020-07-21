4cf3db11e3579c4321edd74ca041ac5cd4358fab
Yue030 <64401885+Yue030@users.noreply.github.com>
2020-07-21 18:03:08 +0800

    CzServers

---
+++ CzServers/src/com/yue/servers/ServerThread.java
@@ -0,0 +1,155 @@
+package com.yue.servers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Server Thread
+ */
+public class ServerThread extends Servers implements Runnable {
+
+    private static final long serialVersionUID = 1L;
+	
+	Socket socket;
+    String socketName;
+    
+    /**
+     * System version
+     */
+    public static final String VERSION = "v3-60996";
+
+    //Safe Lock
+    private static final ReentrantLock printLock = new ReentrantLock(true);
+    private static final ReentrantLock closeLock = new ReentrantLock(true);
+    private static final ReentrantLock refreshLock = new ReentrantLock(true);
+    
+    /**
+     * Constructor
+     * @param socket Socket
+     */
+    public ServerThread(Socket socket) {
+        this.socket = socket;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            socketName = socket.getRemoteSocketAddress().toString();
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            out.println(VERSION);
+            out.flush();
+            refreshIP();
+            String line = reader.readLine();
+            
+            if(line.endsWith(" ~[version]")) {
+            	System.out.println(line);
+            	String[] versionIn = line.split("~");
+            	System.out.println(versionIn[0]);
+            	if(!VERSION.equals(versionIn[0].trim())) {
+            		out.println("shutdown");
+            		out.flush();
+            		sockets.remove(socket);
+            		socket.close();
+            		refreshIP();
+            	} else {
+            		out.println("ok");
+            		out.flush();
+            	}
+            } else {
+            	sockets.remove(socket);
+        		socket.close();
+        		refreshIP();
+            }
+            
+            logArea.append("IP:" + socketName + "\t\u5df2\u958b\u555f\u7ba1\u7406\u7cfb\u7d71\n");
+            boolean flag = true;
+            while (flag){
+                //Connecting
+                line = reader.readLine();
+                //If client exit, disconnect
+                if (line == null){
+                    flag = false;
+                    continue;
+                }
+                
+                if(line.endsWith(" ~[console]")) {
+                	System.out.println(line);
+                	String[] msg = line.split("~");
+                	System.out.println(msg[0] + socket.getRemoteSocketAddress().toString());
+                	logArea.append(msg[0] + "  " + socket.getRemoteSocketAddress().toString() + "\n");
+                } else {
+                	System.out.println("IP:" + socketName + "\t" + line + "\n");
+                    console.append("IP:" + socketName + "\t" + line + "\n");
+                    //Output the message
+                    print(line);
+                }
+                refreshIP();
+            }
+            
+            closeConnect();
+            refreshIP();
+        } catch (IOException e) {
+            try {
+                closeConnect();
+                refreshIP();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    /**
+     * Message to all socket
+     * @param msg Message
+     * @throws IOException IOException
+     */
+    private void print(String msg) throws IOException {
+        PrintWriter out;
+        printLock.lock();
+        try {
+            for (Socket sc : sockets){
+                out = new PrintWriter(sc.getOutputStream());
+                out.println(msg);
+                out.flush();
+            }
+        } finally {
+            printLock.unlock();
+        }
+    }
+    /**
+     * Close the socket connect
+     * @throws IOException IOException
+     */
+    public void closeConnect() throws IOException {
+        closeLock.lock();
+        //Remove sockets
+        try{
+            sockets.remove(socket);
+        } finally {
+            closeLock.unlock();
+        }
+        logArea.append("IP:" + socketName + "\t\u5df2\u95dc\u9589\u7ba1\u7406\u7cfb\u7d71\n");
+
+        socket.close();
+    }
+    
+    /**
+     * Refresh ip area
+     */
+    public void refreshIP() {
+        ip.setText("");
+
+    	refreshLock.lock();
+    	try {
+            for(Socket sc : sockets) {
+                ip.append("IP:" + sc.getRemoteSocketAddress().toString() + "\n");
+            }
+        } finally {
+    	    refreshLock.unlock();
+        }
+    }
+}
---
+++ CzServers/src/com/yue/servers/Servers.java
@@ -0,0 +1,147 @@
+package com.yue.servers;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Servers extends JFrame {
+	public Servers() {
+		
+	}
+	
+	private static final long serialVersionUID = 1L;
+	
+	protected static JTextArea ip = new JTextArea();
+	protected JScrollPane ipScroll = new JScrollPane();
+	
+	protected static JTextArea console = new JTextArea();
+	protected JScrollPane consoleScroll = new JScrollPane();
+	
+	protected JLabel title = new JLabel("\u804A\u5929\u5BA4");
+	protected JLabel ipTitle = new JLabel("\u4F7F\u7528\u8005IP");
+	protected JButton sudoExit = new JButton("\u5F37\u5236\u65B7\u7DDA");
+	
+	protected JScrollPane logScroll = new JScrollPane();
+	protected static JTextArea logArea = new JTextArea();
+	protected JLabel log = new JLabel("\u65E5\u8A8C");
+	
+	protected static final List<Socket> sockets = new Vector<>();
+
+	//Safe Lock
+	private static final ReentrantLock flagLock = new ReentrantLock(true);
+	private static final ReentrantLock exitLock = new ReentrantLock(true);
+
+	public static void main(String[] args) throws IOException {
+		EventQueue.invokeLater(() -> {
+			try {
+				Servers frame = new Servers();
+				frame.initServersUI();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		
+		ServerSocket server = new ServerSocket(5200);
+        boolean flag = true;
+        while (flag){
+            try {
+            	Socket accept = server.accept();
+
+            	flagLock.lock();
+            	try {
+					sockets.add(accept);
+				} finally {
+					flagLock.unlock();
+				}
+            
+            	Thread thread = new Thread(new ServerThread(accept));
+            	thread.start();
+            } catch (Exception e){
+            	flag = false;
+           		e.printStackTrace();
+            }
+        }
+        server.close();
+    }
+
+	public void initServersUI() {
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 809, 530);
+		setTitle("CzServer Version 3-60996");
+		JPanel contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		console.setFont(new Font("Dialog", Font.PLAIN, 12));
+		console.setEditable(false);
+		consoleScroll.setViewportView(console);
+		
+		consoleScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		consoleScroll.setBounds(10, 47, 369, 261);
+		contentPane.add(consoleScroll);
+		
+		ip.setFont(new Font("Dialog", Font.PLAIN, 15));
+		ip.setEditable(false);
+		ipScroll.setViewportView(ip);
+		
+		ipScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		ipScroll.setBounds(10, 372, 369, 111);
+		contentPane.add(ipScroll);
+		
+		
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setFont(new Font("Dialog", Font.PLAIN, 35));
+		title.setBounds(10, 0, 369, 37);
+		contentPane.add(title);
+		
+		ipTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		ipTitle.setFont(new Font("Dialog", Font.PLAIN, 30));
+		ipTitle.setBounds(10, 318, 369, 44);
+		contentPane.add(ipTitle);
+		
+		sudoExit.setFont(new Font("Dialog", Font.PLAIN, 25));
+		sudoExit.setBounds(541, 372, 191, 62);
+		sudoExit.addActionListener(e -> {
+			exitLock.lock();
+
+			try {
+				for(Socket sc : sockets) {
+					try {
+						sc.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+				sockets.clear();
+			} finally {
+				exitLock.unlock();
+			}
+
+			System.out.println(sockets.size());
+		});
+		contentPane.add(sudoExit);
+		
+		
+		logScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		logScroll.setBounds(426, 47, 369, 261);
+		contentPane.add(logScroll);
+		
+		logArea.setFont(new Font("Dialog", Font.PLAIN, 12));
+		logArea.setEditable(false);
+		logScroll.setViewportView(logArea);
+		
+		log.setHorizontalAlignment(SwingConstants.CENTER);
+		log.setFont(new Font("Dialog", Font.PLAIN, 35));
+		log.setBounds(426, 0, 369, 37);
+		contentPane.add(log);
+	}
+}
